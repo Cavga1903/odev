affbbaf012110e564c9303bbab19da79354ea604
Tolga <cavga228@gmail.com>
2025-02-14 02:57:47 +0300

    Add files via upload
    all updated

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-});
+
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("active");
+}
